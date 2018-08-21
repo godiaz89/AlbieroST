5e954da3fc415b15eed10a1f9c6693b506c8aabd
Gonzalo Diaz <gonzalo.diaz@albiero.net>
2018-08-21 14:57:51 -0300

    Initial commit

---
+++ app/src/main/java/net/albiero/albierost/LoginActivity.java
@@ -0,0 +1,14 @@
+package net.albiero.albierost;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
